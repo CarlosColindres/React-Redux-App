ddf3b1d93f4eb083227e89a4d53b878127e31a3d
CarlosColindres <stingaree43@gmail.com>
2020-11-04 20:02:43 -0500

    added more redux

--- src/api/apiUrls.ts
+++
@@ -1,3 +0,0 @@
-export const pokemonSetsUrl = 'https://api.pokemontcg.io/v1/sets'
-
-export const pokemonCardsUrl = 'https://api.pokemontcg.io/v1/cards'
--- src/components/PokemonSets.tsx
+++ src/components/PokemonSets.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import Card from './Card'
 
 const PokemonSets: React.FC <DataTypes> = ({pokemonSets, isLoading}) => {
     console.log(pokemonSets, isLoading)
     return (
         <div className='card-grid'>
-           
+           {pokemonSets.map((card, idx) => <Card key={idx} pokemonCard={card}/>)}
         </div>
     )
 }
